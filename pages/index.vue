4939fc980802123c18fc84b3ea0b8a64bda79233
Eduard Yakovlev <edik25mas@yandex.ru>
2024-05-04 22:28:31 +0300

    22:30

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,77 @@
 
 
 <template>
-  <NuxtLink to="/about">About</NuxtLink>
+  <v-app id="inspire">
+    <v-app-bar
+        class="px-3"
+        density="compact"
+        flat
+    >
+      <v-avatar
+          class="hidden-md-and-up"
+          color="grey-darken-1"
+          size="32"
+      ></v-avatar>
+
+      <v-spacer></v-spacer>
+
+      <v-tabs
+          color="grey-darken-2"
+          centered
+      >
+        <v-tab>
+          <NuxtLink to="/about">About</NuxtLink>
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+
+      <v-avatar
+          class="hidden-sm-and-down"
+          color="grey-darken-1"
+          size="32"
+      ></v-avatar>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+              cols="12"
+              md="2"
+          >
+            <v-sheet
+                min-height="268"
+                rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+              cols="12"
+              md="8"
+          >
+            <v-sheet
+                min-height="70vh"
+                rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+              cols="12"
+              md="2"
+          >
+            <v-sheet
+                min-height="268"
+                rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
